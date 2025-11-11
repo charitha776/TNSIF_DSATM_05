fab5c08eb26866cfc4ab8e599434c7d50e34d950
charitha776 <151713677+charitha776@users.noreply.github.com>
2025-11-11 15:42:10 +0530

    DataTypes.java

--- src/introductiontojavaday1/DataTypes.java
+++ src/introductiontojavaday1/DataTypes.java
@@ -1,14 +1,14 @@
-// Program to demonstrate Data types
+
 package introductiontojavaday1;
 
 public class DataTypes {
 
     public static void main(String[] args) {
 
-        int value1 = 9 / 2; // integer division
-        float value2 = 101 / 61f; // corrected to float division
+        int value1 = 9 / 2; 
+        float value2 = 101 / 61f; 
         double value3 = 10d / 6d;
-        float value4 = 5 / 2.5f; // float division
+        float value4 = 5 / 2.5f; 
 
         System.out.println("value 1 = " + value1); 
         System.out.println("value 2 = " + value2);
@@ -16,8 +16,8 @@ public class DataTypes {
         System.out.println("value 4 = " + value4);
 
         int marker = 512;
-        // assigning expression to variable 
         double percentage = marker * 0.46f;
         System.out.println("percentage: " + percentage);
     }
 }
+
